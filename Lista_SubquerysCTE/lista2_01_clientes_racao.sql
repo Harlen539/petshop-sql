2aa05a21ba655c4f9dacc21eac819d0bfdd355e3
Harlen Henrick <harlenhenrick14@gmail.com>
2026-04-09 17:17:15 -0300

    Add SQL query to select clients who bought 'racao'

---
+++ Lista_SubquerysCTE/lista2_01_clientes_racao.sql
@@ -0,0 +1,9 @@
+SELECT c.nome
+FROM clientes c
+WHERE c.id IN (
+    SELECT v.cliente_id
+    FROM vendas v
+    INNER JOIN itens_venda iv ON v.id = iv.venda_id
+    INNER JOIN produtos p ON iv.produto_id = p.id
+    WHERE LOWER(p.descricao) LIKE '%racao%'
+);
