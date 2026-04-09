2dd092a5c60d8a578f046711a68ca0ace44bdef6
Harlen Henrick <harlenhenrick14@gmail.com>
2026-04-08 22:40:28 -0300

    Add files via upload

---
+++ Lista_Joins/04_produtos_vendidos.sql
@@ -0,0 +1,12 @@
+-- =====================================
+-- 4. PRODUTOS + TOTAL VENDIDO
+-- LEFT JOIN (inclui produtos sem venda)
+-- =====================================
+
+SELECT 
+    p.nome,
+    COALESCE(SUM(iv.quantidade), 0) AS total_vendido
+FROM produtos p
+LEFT JOIN itens_venda iv ON p.id = iv.produto_id
+GROUP BY p.id, p.nome
+ORDER BY total_vendido DESC;
